9f58ccca00fa69b4769f2a499665cbe32790c0e4
admin <rama@gail.com>
2025-03-03 23:59:34 -0800

    new controller greet added

---
+++ src/main/java/com/dhana/app/controller/Greetings.java
@@ -0,0 +1,13 @@
+package com.dhana.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greetings {
+
+    @GetMapping("/greet")
+    public String greetApi() {
+        return "Greetings from SpringBoot Applicaiton";
+    }
+}
